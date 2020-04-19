424d540c9c77325885f5a60bdc90c3c274576430
Chris_Reams <Chris_Reams@unigroup.com>
2020-04-18 23:54:10 -0500

    first commit

---
+++ audioParser/src/test/kotlin/com/chrisreams91/audioParser/AudioParserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chrisreams91.audioParser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AudioParserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
