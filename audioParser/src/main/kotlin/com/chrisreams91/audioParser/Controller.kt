73db412d3d4c080d5622dd5cadcc9affc37c616c
Chris_Reams <Chris_Reams@unigroup.com>
2020-04-26 21:46:56 -0500

    figured out how to execute subproccess that uses pipes

--- audioParser/src/main/kotlin/com/chrisreams91/audioParser/Controller.kt
+++ audioParser/src/main/kotlin/com/chrisreams91/audioParser/Controller.kt
@@ -1,24 +1,24 @@
 package com.chrisreams91.audioParser
 
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 import java.io.File
+import java.io.InputStream
 
 
 @RestController
 @RequestMapping
-class Controller(val audioParserService: AudioParserService) {
+class Controller(val audioParserService: Service) {
 
   @GetMapping("/")
   fun test(): ResponseEntity<String> {
-    val test = audioParserService.parseAudio()
     return ResponseEntity.ok().body("test")
   }
 
-  @PostMapping("/audio")
-  fun audio(@RequestBody file: File) {
-    audioParserService.parseAudio()
+  @PostMapping("/")
+  fun audio(@RequestParam(value = "audio") audioFile: MultipartFile) {
+    audioParserService.parseAudio(audioFile)
     ResponseEntity.ok()
   }
   
--- audioParser/src/main/kotlin/com/chrisreams91/audioParser/Service.kt
+++ audioParser/src/main/kotlin/com/chrisreams91/audioParser/Service.kt
@@ -1,26 +1,38 @@
 package com.chrisreams91.audioParser
 
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 import java.io.File
+import java.nio.charset.Charset
 import java.util.concurrent.TimeUnit
 
 
 @Service
-class AudioParserService {
+class Service {
 
-  fun parseAudio(): List<String> {
+  fun parseAudio(file: MultipartFile): List<String> {
     val words = mutableListOf<String>()
 
-    // need to figure out the right path to deepspeech and if i can just use bytes or need a file
-    val process = ProcessBuilder("echo", "path/to/the/file").start()
+    val processBuilders = mutableListOf(
+      ProcessBuilder("deepspeech", "--model", "deepspeech-0.7.0-models.pbmm", "--audio", "audio/2830-3980-0043.wav")
+        .directory(File("/Users/tcxr3")),
+      ProcessBuilder("cat")
+    )
 
-    process.inputStream.reader(Charsets.UTF_8).use {
+    val processes: List<Process> = ProcessBuilder.startPipeline(processBuilders)
+    val first = processes.first()
+    val last = processes.last()
+
+    first.waitFor(10, TimeUnit.SECONDS)
+    last.waitFor(10, TimeUnit.SECONDS)
+
+    // deepspeech --model deepspeech-0.7.0-models.pbmm --audio audio/2830-3980-0043.wav | cat
+    last.inputStream.reader(Charsets.UTF_8).use {
       it.readText().split(" ").forEach { word ->
         words.add(word)
       }
     }
 
-    process.waitFor(10, TimeUnit.SECONDS)
     return words
   }
 
