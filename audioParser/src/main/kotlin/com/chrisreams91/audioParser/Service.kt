8c0769209844fb831a7719267263c7e9db383d51
Chris_Reams <Chris_Reams@unigroup.com>
2020-04-27 01:17:00 -0500

    saves the audio file sent in request and runs deepspeech off of it

--- audioParser/src/main/kotlin/com/chrisreams91/audioParser/Service.kt
+++ audioParser/src/main/kotlin/com/chrisreams91/audioParser/Service.kt
@@ -3,18 +3,29 @@ package com.chrisreams91.audioParser
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
-import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.concurrent.TimeUnit
 
 
 @Service
 class Service {
 
+  private val projectRootDirectory = System.getProperty("user.dir")
+
+  fun saveAudioFile(file: MultipartFile): Path {
+    val path = Paths.get("${projectRootDirectory}/src/main/kotlin/com/chrisreams91/audioParser/Audio/temp.wav")
+    Files.write(path, file.bytes)
+    return path
+  }
+
   fun parseAudio(file: MultipartFile): List<String> {
     val words = mutableListOf<String>()
+    val filePath = saveAudioFile(file)
 
     val processBuilders = mutableListOf(
-      ProcessBuilder("deepspeech", "--model", "deepspeech-0.7.0-models.pbmm", "--audio", "audio/2830-3980-0043.wav")
+      ProcessBuilder("deepspeech", "--model", "deepspeech-0.7.0-models.pbmm", "--audio", filePath.toString())
         .directory(File("/Users/tcxr3")),
       ProcessBuilder("cat")
     )
@@ -26,11 +37,14 @@ class Service {
     first.waitFor(10, TimeUnit.SECONDS)
     last.waitFor(10, TimeUnit.SECONDS)
 
-    // deepspeech --model deepspeech-0.7.0-models.pbmm --audio audio/2830-3980-0043.wav | cat
     last.inputStream.reader(Charsets.UTF_8).use {
-      it.readText().split(" ").forEach { word ->
-        words.add(word)
-      }
+      it
+        .readText()
+        .replace("\n", "")
+        .split(" ")
+        .forEach { word ->
+          words.add(word)
+        }
     }
 
     return words
