54cfee3fdfc70351200107331827f8c894fb09d8
Chris_Reams <Chris_Reams@unigroup.com>
2020-07-13 00:02:27 -0500

    set up database connections

--- audioParser/src/main/kotlin/com/chrisreams91/audioParser/model/Audio.kt
+++ audioParser/src/main/kotlin/com/chrisreams91/audioParser/model/Audio.kt
@@ -1,9 +1,24 @@
 package com.chrisreams91.audioParser.model
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType
+import org.hibernate.annotations.Type
+import org.hibernate.annotations.TypeDef
 import java.time.Instant
 import java.util.*
+import javax.persistence.*
 
+@Entity
+@Table(name = "words")
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType::class)
 data class Audio(
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  val id : Long = 0,
+
+  @Type(type = "jsonb")
+  @Column(columnDefinition = "jsonb")
   private val words: List<String> = emptyList(),
-  private val timestamp: Date = Date.from(Instant.now())
+
+  private val creation_time: Date = Date.from(Instant.now())
 )
--- audioParser/src/test/kotlin/com/chrisreams91/audioParser/AudioParserApplicationTests.kt
+++ audioParser/src/test/kotlin/com/chrisreams91/audioParser/AudioParserApplicationTests.kt
@@ -1,6 +1,6 @@
 package com.chrisreams91.audioParser
 
-import org.junit.jupiter.api.Test
+import org.junit.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
